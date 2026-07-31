f4feb3004a42d81d4091642d12a73334aa028f9b
Niraj Raut <1751nk@gmail.com>
2026-07-31 10:12:29 +0530

    refactored

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
 import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+// import './index.css'
 import App from './App.jsx'
+import ReactDOM from "react-dom";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.render(
+  <App />,
+  document.getElementById("root")
+);
